95444331b3b998bac25c6762e9e046a0d9370059
Lim Meng Kiat <mengkiatlim@gmail.com>
2021-02-22 06:53:19 +0000

    connect to janus  server through websocket

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 
+import ssl
 import asyncio
 import websockets
 
+# ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+# ssl_context.check_hostname = False
+# ssl_context.verify_mode = ssl.CERT_NONE
+
 class JanusClient:
-    def __init__(self):
-        pass
+    def __init__(self, uri: str = ""):
+        self.uri = uri
 
-    def connect(self):
-        pass
+    async def connect(self) -> None:
+        print("Connecting to: ", self.uri)
+        # self.ws = await websockets.connect(self.uri, ssl=ssl_context)
+        self.ws = await websockets.connect(self.uri, subprotocols=["janus-protocol"])
+        print("Connected")
 
-    def disconnect(self):
-        pass
+    async def disconnect(self):
+        print("Disconnecting")
+        await self.ws.close()
+
+async def main():
+    client = JanusClient("ws://lt.limmengkiat.name.my/janusws/")
+    await client.connect()
+    await client.disconnect()
+    print("End of main")
+
+asyncio.run(main())
