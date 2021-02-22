a46cba5eaa0d92952c13f8e72df106048c3e138f
Lim Meng Kiat <mengkiatlim@gmail.com>
2021-02-22 07:15:07 +0000

    attach and detach plugin

--- main.py
+++ main.py
@@ -25,17 +25,40 @@ class JanusClient:
 async def main():
     client = JanusClient("ws://lt.limmengkiat.name.my/janusws/")
     await client.connect()
+    # Create session
     await client.ws.send(json.dumps({
         "janus": "create",
-        "transaction": "123"
+        "transaction": "1234"
     }))
     response = json.loads(await client.ws.recv())
     print(response)
     if response["janus"] == "success":
+        # Session created
+        # Attach plugin
+        await client.ws.send(json.dumps({
+            "janus": "attach",
+            "transaction": "1235",
+            "session_id": response["data"]["id"],
+            "plugin": "janus.plugin.echotest",
+        }))
+        response_plugin = json.loads(await client.ws.recv())
+        print(response_plugin)
+        if response_plugin["janus"] == "success":
+            # Plugin attached
+            # Destroy plugin
+            await client.ws.send(json.dumps({
+                "janus": "detach",
+                "transaction": "1236",
+                "session_id": response["data"]["id"],
+                "handle_id": response_plugin["data"]["id"],
+            }))
+            print(await client.ws.recv())
+            print(await client.ws.recv())
+        # Destroy session
         await client.ws.send(json.dumps({
             "janus": "destroy",
-            "transaction": "123",
-            "session_id": response["data"]["id"]
+            "transaction": "1237",
+            "session_id": response["data"]["id"],
         }))
         print(await client.ws.recv())
     await client.disconnect()
