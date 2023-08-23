a125714e9d0d0233bf1ca6db355256cbf2d23086
Joseph Lim <josephlim_94@hotmail.co.uk>
2023-08-23 21:51:57 +0800

    add github star button to RTD

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Janus Client'
-copyright = '2021, Lim Meng Kiat'
-author = 'Lim Meng Kiat'
+project = "Janus Client"
+copyright = "2021, Lim Meng Kiat"
+author = "Lim Meng Kiat"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,16 +29,16 @@ author = 'Lim Meng Kiat'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -45,11 +46,55 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
+
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
+        "donate.html",
+    ]
+}
+
+html_theme_options = {
+    "description": "Janus WebRTC gateway Python asyncio client",
+    "canonical_url": "https://janus-client-in-python.readthedocs.io/",
+    "github_user": "josephlim94",
+    "github_repo": "janus_gst_client_py",
+    "github_button": True,
+    "github_type": "star",
+    "github_banner": True,
+    # "badges": [
+    #     {
+    #         "image": "https://img.shields.io/badge/License-MIT-yellow.svg",
+    #         "target": "https://opensource.org/licenses/MIT",
+    #         "height": "20",
+    #         "alt": "License: MIT",
+    #     },
+    #     {
+    #         "image": "https://img.shields.io/badge/Stage-ALPHA-orange.svg",
+    #         # "target": f"https://badge.fury.io/py/{project}",
+    #         "height": "20",
+    #         "alt": "Development Stage",
+    #     },
+    #     {
+    #         "image": "https://readthedocs.org/projects/janus-client-in-python/badge/?version=latest",
+    #         "target": "https://janus-client-in-python.readthedocs.io/en/latest/?badge=latest",
+    #         "height": "20",
+    #         "alt": "Documentation Status",
+    #     },
+    #     {
+    #         "image": "https://img.shields.io/badge/coverage-80%25-green",
+    #         # "target": f"https://codecov.io/github/{github_repo_slug}",
+    #         "height": "20",
+    #         "alt": "Code coverage status",
+    #     },
+    # ],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-autodoc_mock_imports = ['gi']
+html_static_path = ["_static"]
