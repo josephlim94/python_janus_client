d359aafa3c4d742da6523fefff254b309c216a6d
Joseph Lim <josephlim_94@hotmail.co.uk>
2023-09-23 23:15:29 +0800

    must propagate test errors

--- test/util.py
+++ test/util.py
@@ -1,9 +1,4 @@
 import asyncio
-import logging
-
-format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-logger = logging.getLogger()
 
 
 def async_test(coro):
@@ -11,8 +6,6 @@ def async_test(coro):
         loop = asyncio.new_event_loop()
         try:
             return loop.run_until_complete(coro(*args, **kwargs))
-        except Exception as err:
-            logger.error(err)
         finally:
             loop.close()
 
