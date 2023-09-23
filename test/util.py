a12d68154d935a0c500698d3bb65f5433c39c2d9
Joseph Lim <josephlim_94@hotmail.co.uk>
2023-09-23 22:32:57 +0800

    log error in async_test

--- test/util.py
+++ test/util.py
@@ -1,4 +1,9 @@
 import asyncio
+import logging
+
+format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+logger = logging.getLogger()
 
 
 def async_test(coro):
@@ -6,6 +11,8 @@ def async_test(coro):
         loop = asyncio.new_event_loop()
         try:
             return loop.run_until_complete(coro(*args, **kwargs))
+        except Exception as err:
+            logger.error(err)
         finally:
             loop.close()
 
