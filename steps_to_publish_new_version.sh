b99d7bbf75c66f72a57ce3c2faf7ddc1f40b0b77
Joseph Lim <josephlim_94@hotmail.co.uk>
2025-10-20 23:46:00 +0800

    Release v0.8.1

--- steps_to_publish_new_version.sh
+++ steps_to_publish_new_version.sh
@@ -7,10 +7,10 @@ hatch test -i py=3.8 -c  # All tests must pass
 hatch env run -e py3.8 coverage report -- --format=total  # Update total into README.md
 
 # Set new version number in pyproject.toml
-git add pyprojet.toml README.md
+git add pyproject.toml README.md
 git commit -m "Release v${hatch version}"
 git push
 git tag -a release-${hatch version} -m "Release v${hatch version}"
 git push origin release-${hatch version}
 
-
+# Really build and release it
